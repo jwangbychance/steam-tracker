15e7861a24df241cc3ceae4b012dfa7789a23288
jwangbychance <jackson.wang1998@outlook.com>
2023-12-06 22:41:25 +1100

    add unit test for steam recent games api call

---
+++ tests/unit/api/steamRecentGames.unit.test.ts
@@ -0,0 +1,19 @@
+import { fetchGamesData } from "../../../src/utils/api";
+import axios from "axios";
+
+describe("steam recent games api call tests", () => {
+  afterEach(() => {
+    jest.clearAllMocks;
+  });
+
+  describe("get recent steam games", () => {
+    const axiosGetSpy = jest.spyOn(axios, "get");
+
+    it("should return user steam recent games", async () => {
+      await fetchGamesData("76561198028760616");
+      expect(axiosGetSpy).toHaveBeenCalledWith("/api/steamRecentGames", {
+        params: { steamId: "76561198028760616" },
+      });
+    });
+  });
+});

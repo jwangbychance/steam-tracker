7f4d896260c180e6507fd6e42ba3384f25dd07d3
jwangbychance <jackson.wang1998@outlook.com>
2023-12-06 22:49:18 +1100

    add unit test for steam user friends api call

---
+++ tests/unit/api/steamUserFriends.unit.test.ts
@@ -0,0 +1,19 @@
+import { fetchUserFriends } from "../../../src/utils/api";
+import axios from "axios";
+
+describe("steam user friends api call tests", () => {
+  afterEach(() => {
+    jest.clearAllMocks;
+  });
+
+  describe("get user steam friends", () => {
+    const axiosGetSpy = jest.spyOn(axios, "get");
+
+    it("should return user steam friends", async () => {
+      await fetchUserFriends("76561198028760616");
+      expect(axiosGetSpy).toHaveBeenCalledWith("/api/steamUserFriends", {
+        params: { steamId: "76561198028760616" },
+      });
+    });
+  });
+});

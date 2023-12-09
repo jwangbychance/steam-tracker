b5b45e89972da95ed15c29ea73ff420c2032c256
jwangbychance <jackson.wang1998@outlook.com>
2023-12-10 00:05:02 +1100

    add test for rendering achievements when data is provided

--- tests/unit/components/Achievements.unit.test.tsx
+++ tests/unit/components/Achievements.unit.test.tsx
@@ -1,10 +1,25 @@
 import Achievements from "../../../src/components/Achievements";
 import { render } from "@testing-library/react";
+import { ISteamAchievements } from "../../../src/interfaces/ISteamAchievements";
 
 describe("Achievements", () => {
   it("should not render when data is not provided", () => {
     const { container } = render(<Achievements achievementsData={null} />);
 
     expect(container.firstChild).toBeNull();
+  });
+
+  it("should render when data is provided", () => {
+    const mockData: {
+      [name: string]: ISteamAchievements[];
+    } = {
+      "Some Game Title": [{ name: "some_achievement", achieved: 1 }],
+    };
+
+    const { queryByText } = render(
+      <Achievements achievementsData={mockData} />
+    );
+
+    expect(queryByText("some_achievement")).not.toBeNull();
   });
 });

39ff47ad41c656a7a69a994c09b13ed91195fdf0
jwangbychance <jackson.wang1998@outlook.com>
2023-12-10 00:32:56 +1100

    add test for Footer component to handle opening link in new tab

--- tests/unit/components/Footer.unit.test.tsx
+++ tests/unit/components/Footer.unit.test.tsx
@@ -1,10 +1,32 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import Footer from "../../../src/components/Footer";
 
+let windowSpy;
+
 describe("Footer", () => {
+  beforeEach(() => {
+    windowSpy = jest.spyOn(window, "location", "get");
+  });
+
+  afterEach(() => {
+    windowSpy.mockRestore();
+  });
+
   it("should render", () => {
     const { queryByText } = render(<Footer />);
 
     expect(queryByText("jwangbychance@github")).not.toBeNull();
+  });
+
+  it("should open the correct URL when the tag is pressed", () => {
+    const { queryByText } = render(<Footer />);
+
+    fireEvent.click(queryByText("jwangbychance@github"));
+
+    windowSpy.mockImplementation(() => ({
+      origin: "https://github.com/jwangbychance",
+    }));
+
+    expect(window.location.origin).toEqual("https://github.com/jwangbychance");
   });
 });

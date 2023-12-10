8a00600b4456fe76d5acfe960790832a1b14fc62
jwangbychance <jackson.wang1998@outlook.com>
2023-12-11 01:24:49 +1100

    add test for rendering games when data is provided

--- tests/unit/components/GameCardLayout.unit.test.tsx
+++ tests/unit/components/GameCardLayout.unit.test.tsx
@@ -1,10 +1,30 @@
 import { render } from "@testing-library/react";
 import GameCardLayout from "../../../src/components/GameCardLayout";
+import { ISteamGame } from "../../../src/interfaces/ISteamGame";
 
 describe("GameCardLayout", () => {
   it("should not render when data is not provided", () => {
     const { container } = render(<GameCardLayout gamesData={null} />);
 
     expect(container.firstChild).toBeNull();
+  });
+
+  it("should render when data is provided", () => {
+    const mockData: ISteamGame[] = [
+      {
+        appid: 123,
+        img_icon_url: "some_url",
+        name: "Some Game",
+        playtime_2weeks: 123,
+        playtime_forever: 123,
+        playtime_linux_forever: 123,
+        playtime_mac_forever: 123,
+        playtime_windows_forever: 123,
+      },
+    ];
+
+    const { queryByText } = render(<GameCardLayout gamesData={mockData} />);
+
+    expect(queryByText("Some Game")).not.toBeNull();
   });
 });

91b788ac95acb043befebeacdbbccd56ba361207
jwangbychance <jackson.wang1998@outlook.com>
2023-12-14 00:02:17 +1100

    add test for expecting user steam achievements data properties

--- tests/unit/api/steamUserAchievements.test.ts
+++ tests/unit/api/steamUserAchievements.test.ts
@@ -1,3 +1,4 @@
+import { ISteamAchievements } from "../../../src/interfaces/ISteamAchievements";
 import { fetchUserAchievements } from "../../../src/utils/api";
 import axios from "axios";
 
@@ -9,11 +10,25 @@ describe("steam user achievements api call tests", () => {
   describe("get user steam achievements", () => {
     const axiosGetSpy = jest.spyOn(axios, "get");
 
-    it("should return user steam achievements", async () => {
+    it("should make a correct API call to retrieve user steam achievements", async () => {
       await fetchUserAchievements(1337520);
       expect(axiosGetSpy).toHaveBeenCalledWith("/api/steamUserAchievements", {
         params: { appId: 1337520 },
       });
+    });
+
+    it("should return user steam achievements data", async () => {
+      const mockResponseData: ISteamAchievements = {
+        name: "some_achievements",
+        achieved: 1,
+      };
+
+      axiosGetSpy.mockResolvedValueOnce({ data: mockResponseData });
+
+      const result = await fetchUserAchievements(1337520);
+
+      expect(result).toHaveProperty("name");
+      expect(result).toHaveProperty("achieved");
     });
   });
 });
